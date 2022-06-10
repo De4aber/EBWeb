b7d3276860fd948c0d49aefa85930514a350a9e7
Frann0 <deengo113@gmail.com>
2022-06-10 17:20:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
             <Route path="/" element={
               <>
                 <FrontPage/>
-                <Navbar />
               </>
             } />
           </Routes>
